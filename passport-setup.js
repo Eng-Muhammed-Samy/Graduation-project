93bc519aba6f47f23e77c2a6d609ef5b781d6d4d
Eng-Muhammed-Samy <eng_muhamed_samy@yahoo.com>
2021-03-23 21:06:27 +0200

    akher edit

--- passport-setup.js
+++ passport-setup.js
@@ -1,71 +1,73 @@
-const passport = require("passport");
-const connection = require("./models/init_database").connection;
+/** @format */
+
+const passport = require('passport');
+const connection = require('./models/init_database').connection;
 
 function createUser(name, email) {
-  return new Promise((res, rej) => {
-    connection.query(
-      "SELECT * FROM accounts WHERE email = $1",
-      [email],
-      function (error, results, fields) {
-        if (results.rows.length > 0) {
-          res({"id":results.rows[0].id,"profileimg":results.rows[0]['img_url']});
-        } else {
-          connection.query(
-            "INSERT INTO accounts (username,Email) VALUES ($1,$2)",
-            [name, email],
-            function (err, result) {
-              if (err) rej(err);
-              res();
-            }
-          );
-        }
-      }
-    );
-  });
+	return new Promise((res, rej) => {
+		connection.query(
+			'SELECT * FROM accounts WHERE email = $1',
+			[email],
+			function (error, results, fields) {
+				if (results.rows.length > 0) {
+					res({
+						id: results.rows[0].id,
+						profileimg: results.rows[0]['img_url'],
+					});
+				} else {
+					connection.query(
+						'INSERT INTO accounts (username,Email) VALUES ($1,$2)',
+						[name, email],
+						function (err, result) {
+							if (err) rej(err);
+							res();
+						},
+					);
+				}
+			},
+		);
+	});
 }
 
 passport.serializeUser(function (user, done) {
-  done(null, user);
+	done(null, user);
 });
 
 passport.deserializeUser(function (user, done) {
-
-  // User.findById(id, function (err, user) {
-  done(null, user);
-  // });
+	// User.findById(id, function (err, user) {
+	done(null, user);
+	// });
 });
 
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+// mohammed hassan
 passport.use(
-  new GoogleStrategy(
-    {
-      clientID:
-        "1045211242467-8o7muise14ohj3g76nb699f0tifqe5l1.apps.googleusercontent.com",
-      clientSecret: "Vp-s7DWKrqI5x8aBh2z4__h7",
-      callbackURL: "http://localhost:3000/google/callback",
-    },
-    async function (accessToken, refreshToken, profile, done) {
-      /*
+	new GoogleStrategy(
+		{
+			clientID:
+				'1045211242467-8o7muise14ohj3g76nb699f0tifqe5l1.apps.googleusercontent.com',
+			clientSecret: 'Vp-s7DWKrqI5x8aBh2z4__h7',
+			callbackURL: 'http://localhost:3000/google/callback',
+		},
+		async function (accessToken, refreshToken, profile, done) {
+			/*
      use the profile info (mainly profile id) to check if the user is registerd in ur db
      If yes select the user and pass him to the done callback
      If not create the user and then select him and pass to callback
     */
-      const email = profile.emails[0].value;
-      const name = profile.displayName;
+			const email = profile.emails[0].value;
+			const name = profile.displayName;
 
-      createUser(name, email)
-        .then((obj) => {
-          profile.appId = obj.id;
-          profile.profileimg=obj.profileimg;
-        })
-        .then(() => {
-          done(null, profile);
-        })
-        .catch((e) => {
-
-        });
-
-    }
-  )
+			createUser(name, email)
+				.then((obj) => {
+					profile.appId = obj.id;
+					profile.profileimg = obj.profileimg;
+				})
+				.then(() => {
+					done(null, profile);
+				})
+				.catch((e) => {});
+		},
+	),
 );
